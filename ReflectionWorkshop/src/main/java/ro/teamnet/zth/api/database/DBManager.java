742a873655aa0acab4c566f7f4607d3844a088e2
Alexandra Lazea <medeealazea@yahoo.com>
2016-07-08 11:54:08 +0300

    Proiect

---
+++ ReflectionWorkshop/src/main/java/ro/teamnet/zth/api/database/DBManager.java
@@ -0,0 +1,48 @@
+package ro.teamnet.zth.api.database;
+
+import java.sql.*;
+
+public class DBManager {
+
+    private static final String CONNECTION_STRING = "jdbc:oracle:thin:@" + DBProperties.IP + ":" + DBProperties.PORT;
+
+    private DBManager() throws UnsupportedOperationException{
+    }
+
+    private static void registerDriver(){
+        try {
+            Class.forName(DBProperties.DRIVER_CLASS);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection(){
+        registerDriver();
+        try {
+            return DriverManager.getConnection(CONNECTION_STRING,DBProperties.USER, DBProperties.PASS);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void checkConnection(Connection connection){
+        Connection conn = getConnection();
+        try{
+            Statement st = conn.createStatement();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        String SQL = "select 1 from dual";
+        try{
+            PreparedStatement pst = conn.prepareStatement(SQL);
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ ReflectionWorkshop/src/main/java/ro/teamnet/zth/api/database/DBProperties.java
@@ -0,0 +1,10 @@
+package ro.teamnet.zth.api.database;
+
+public interface DBProperties {
+
+    String IP = "127.0.0.1";
+    String PORT = "49161";
+    String USER = "sys as sysdba";
+    String PASS = "oracle";
+    String DRIVER_CLASS = "oracle.jdbc.driver.OracleDriver";
+}
