cf5f74f489bf2f49e5fadaaad732c812f7216ce1
Alexandru.Dinca <alexandru.dinca@teamnet.ro>
2016-07-08 13:33:34 +0300

    update EntityTest

--- ReflectionWorkshop/src/main/java/ro/teamnet/zth/api/em/EntityManagerImpl.java
+++ ReflectionWorkshop/src/main/java/ro/teamnet/zth/api/em/EntityManagerImpl.java
@@ -10,11 +10,25 @@ import java.util.List;
  * Created by Alexandru.Dinca on 7/8/2016.
  */
 public class EntityManagerImpl implements EntityManager {
-
-
-
     Connection conn = null;
 
+/*
+* -	create a connection to DB;
+-	get table name, columns and fields by annotations using the methods from EntityUtils class;
+-	create a Condition object in which you have to set column name and the value of the id;
+-	create a QueryBuilder object  in which you have to set the name of the table, columns, query type and conditions;
+-	call createQuery() method from QueryBuilder.java;
+-	create a resultSet object using Statement and execute the query obtained above;
+-	if the resultSet has any value (resultSet.next()) then:
+o	 you have to create an instance of type T;
+o	iterate through ColumnInfo list and obtain the field of the instance using getColumnName().
+Ex: instance.getClass().getDeclaredField(column.getColumnName());
+o	make the field accessible;
+o	set the value of the field with the value obtained from resultSet object;
+
+-	return the instance;
+
+* */
     @Override
     public <T> T findById(Class<T> entityClass, Long id) {
         conn = DBManager.getConnection();
@@ -25,6 +39,11 @@ public class EntityManagerImpl implements EntityManager {
 
             }
         }
+//        -	get table name, columns and fields by annotations using the methods from EntityUtils class;
+
+
+
+
         return null;
     }
 
---
+++ ReflectionWorkshop/src/test/java/em/EntityManagerImplTest.java
@@ -0,0 +1,31 @@
+package em;
+
+import org.junit.Test;
+
+/**
+ * Created by Alexandru.Dinca on 7/8/2016.
+ */
+public class EntityManagerImplTest {
+//    <T> T findById(Class<T> entityClass, Long id);
+    @Test
+    public void testFindById(){}
+
+    @Test
+    public void testGetNextIdVal(String tableName, String columnIdName){
+
+    }
+
+//    <T> Object insert(T entity);
+    @Test
+    public void testInsert(){
+
+    }
+
+
+//    <T> List<T> findAll(Class<T> entityClass);
+    @Test
+    public void testFindAll(){
+
+    }
+
+}
